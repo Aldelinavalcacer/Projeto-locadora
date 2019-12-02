b5ea1685af8083a8714b1d3a67f96e76d22aab41
laura <laura@DESKTOP-1QRJJRA>
2019-12-02 15:58:59 -0200

    Tela de login

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DAO;
 
-/**
- *
- * @author Aldel
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
 public class Conexao {
+    public static Connection AbrirConexao(){
+        Connection con = null;
+        try {
+            Class.forName("com.sql.jdbc.Driver");
+            String url= "jdbc:msql://localhost/locadora";
+            con = DriverManager.getConnection (url, "root", "123");
+        } catch (Exception e){
+            JOptionPane.showMessageDialog (null, "Erro na conexão com o banco",
+                    "Video Locadora", JOptionPane.ERROR_MESSAGE);
+            e.getMessage();
+        }
+        return con;
+    }
+    public static void FecharConexao (Connection con){
+        try {
+            con.close();
+        } catch (Exception e){
+            System.out.println (e.getMessage());
+        }
+    }
     
 }
