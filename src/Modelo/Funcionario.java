2358e9b32f0a64bcadb9fd8779ffb91c1424dede
laura <laura@DESKTOP-1QRJJRA>
2019-12-04 08:50:55 -0200

    Getters e setters

--- src/Modelo/Funcionario.java
+++ src/Modelo/Funcionario.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
-/**
- *
- * @author Aldel
- */
+
 public class Funcionario {
+    private int cod;
+    private String nome;
+    private String Login;
+    private String senha;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String Login) {
+        this.Login = Login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
     
 }
