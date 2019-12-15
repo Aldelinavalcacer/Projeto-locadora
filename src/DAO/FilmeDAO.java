f008bb8b98192a3569089fb2f444a96977755af4
laura <laura@DESKTOP-1QRJJRA>
2019-12-15 20:57:39 -0200

    {Laura} Conclusão de mais códigos (filme, funcionário)

--- src/DAO/FilmeDAO.java
+++ src/DAO/FilmeDAO.java
@@ -1,14 +1,230 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import Modelo.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Aldel
+ * @author Laura
  */
-public class FilmeDAO {
+public class FilmeDAO extends ExecuteSQL {
     
+    public FilmeDAO(Connection con) {
+        super(con);
+    }
+    
+    public String Inserir_Filme(Filme a){
+        try{
+            String sql = "insert into filme values(0,?,?,?,?,?,?)";
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            
+            ps.setString(1, a.getTitulo());
+            ps.setInt(2, a.getAno());
+            ps.setString(3, a.getDuracao());
+            ps.setInt(4, a.getCod_categoria());
+            ps.setInt(5, a.getCod_classificacao());
+            ps.setString(6, a.getCapa());
+            
+            if (ps.executeUpdate() > 0) {
+                return "Inserido com sucesso";
+            } else {
+                return "Erro ao inserir!";
+            }
+        }catch(SQLException e){
+            return e.getMessage();
+        }
+    }
+    
+    public List<Filme> ListarComboFilme(){
+        String sql = "select titulo from filme order by titulo";
+        List<Filme> lista = new ArrayList<>();
+        
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs != null) {
+                while (rs.next()) {
+                    Filme a = new Filme();
+                    a.setTitulo(rs.getString(1));
+                    lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public List<Filme> ConsultaCodigoFilme(String nome) {
+        String sql = "select idfilme from filme where titulo = '"+ nome +"'";
+        List<Filme> lista = new ArrayList<>();
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs != null) {
+                while (rs.next()) {
+                    Filme a = new Filme();
+                    a.setCodigo(rs.getInt(1));
+                    lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public List<Filme> ConsultaNomeFilme(int cod) {
+        String sql = "select titulo, capa from filme where idfilme = "+ cod +"";
+        List<Filme> lista = new ArrayList<>();
+        try {
+            
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    Filme a = new Filme();
+                    a.setTitulo(rs.getString(1));
+                    a.setCapa(rs.getString(2));
+                    lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public String Alterar_Filme(Filme a) {
+    String sql = "update filme set titulo = ? ,ano = ? ,duracao = ? "
+            + ",cod_categoria = ? ,cod_classificacao = ? ,capa = ?";
+    try {
+        PreparedStatement ps = getCon() .prepareStatement(sql);
+        ps.setString(1, a.getTitulo());
+        ps.setInt(2, a.getAno());
+        ps.setString(3, a.getDuracao());
+        ps.setInt(4, a.getCod_categoria());
+        ps.setInt(5, a.getCod_classificacao());
+        ps.setString(6, a.getCapa());
+        ps.setInt(7, a.getCodigo());
+        if (ps.executeUpdate() > 0) {
+            return "Atualizado com sucesso.";
+        } else {
+            return "Erro ao Atualizar";
+        }
+    } catch (SQLException e) {
+        return e.getMessage();
+    }
 }
+
+    public List<Filme> ListarFilme() {
+        String sql = "select idfilme,titulo,ano,duracao,cod_categoria,cod_classificacao from filme";
+        List<Filme> lista = new ArrayList<>();
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs != null) {
+                while (rs.next()) {
+            Filme a = new Filme();
+            a.setCodigo(rs.getInt(1));
+            a.setTitulo(rs.getString(2));
+            a.setAno(rs.getInt(3));
+            a.setDuracao(rs.getString(4));
+            a.setCod_categoria(rs.getInt(5));
+            a.setCod_classificacao(rs.getInt(6));
+            a.setCapa(rs.getString((7)));
+            
+            lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+       } catch (SQLException e) {
+           return null; 
+       }
+   
+    }
+    
+    public boolean Testar_Filme(int cod) {
+            boolean Resultado = false;
+            try {
+                
+                String sql = "select * from filme where idfilme = " + cod + "";
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                
+                if (rs != null) {
+                    while (rs.next()) {
+                        Resultado = true;
+                    }
+                }
+            } catch (SQLException ex) {
+                ex.getMessage();
+            }
+            return Resultado;
+      }
+    
+    
+    public List<Filme> CapturarFilme(int cod) {
+            String sql = "select * from filme where idfilme =" + cod + " ";
+            List<Filme> lista = new ArrayList<>();
+            try {
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                if (rs != null) {
+                    while (rs.next()) {
+                        Filme a = new Filme();
+                        a.setCodigo(rs.getInt(1));
+                        a.setTitulo(rs.getString(2));
+                        a.setAno(rs.getInt(3));
+                        a.setDuracao(rs.getString(4));
+                        a.setCod_categoria(rs.getInt(5));
+                        a.setCod_classificacao(rs.getInt(6));
+                        a.setCapa(rs.getString(7));
+                        lista.add(a);
+                    }
+                    return lista;
+                 } else {
+                    return null;
+                }
+            } catch (SQLException e) {
+                return null;
+            }
+        }
+    
+    public String Excluir_Filme(Filme a){
+        String sql = "delete from filme where idfilme = ? and titulo = ?";
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setInt(1, a.getCodigo());
+            ps.setString(2, a.getTitulo());
+            if (ps.executeUpdate() > 0) {
+                return "Excluido com sucesso";
+            } else {
+                return "Erro ao excluir";
+            }
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
+
+    public List<Filme> Pesquisar_Cod_Filme(int codigo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
--- src/DAO/FuncionarioDAO.java
+++ src/DAO/FuncionarioDAO.java
@@ -1,37 +1,180 @@
-
 package DAO;
 
-import Modelo.Funcionario;
+import Modelo.Cliente;
 import java.sql.*;
+import Modelo.Funcionario;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FuncionarioDAO extends ExecuteSQL{
+public class FuncionarioDAO extends ExecuteSQL {
     
-    public FuncionarioDAO (Connection con) {
+    public FuncionarioDAO(Connection con) {
         super(con);
     }
-    public boolean Logar (String login, String senha){
+    public boolean Logar(String login, String senha) {
         boolean finalResult = false;
-        try{
-            String consulta ="select login, senha from funcionario where login = '" + login + " 'and senha = '" + senha + "'";
-
+        try {
+            String consulta = "select login, senha from funcionario "
+          + "where login = '" + login + "' and senha = '" + senha + "'";
             PreparedStatement ps = getCon().prepareStatement(consulta);
             ResultSet rs = ps.executeQuery();
             
-            if (rs != null){
-                while (rs.next()){
-                Funcionario a = new Funcionario ();
-                a.setLogin (rs.getString(1));
-                a.setSenha (rs.getString(2));
-                finalResult = true;
+            if (rs != null) {
+                while (rs.next()) {
+                    Funcionario a = new Funcionario();
+                    a.setLogin(rs.getString(1));
+                    a.setSenha(rs.getString(2));
+                    finalResult = true;
+                }
             }
-                        
-         }
-                    
         } catch (SQLException ex) {
             ex.getMessage();
         }
-         return finalResult;
+        return finalResult;
+    }
     
+    public String Inserir_Funcionario(Funcionario a) {
+        try {
+            String sql = "insert into funcionario values(0,?,?,?)";
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setString(1, a.getNome());
+            ps.setString(2, a.getLogin());
+            ps.setString(3, a.getSenha());
+            
+            if (ps.executeUpdate() > 0) {
+                return "Inserido com sucesso!";
+            } else {
+                return "Erro ao inserir";
+            }
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
+
+     public List<Funcionario> ListarComboFuncionario(){
+        String sql = "select nome from funcionario order by nome";
+        List<Funcionario> lista = new ArrayList<>();
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    Funcionario a = new Funcionario();
+                    a.setNome(rs.getString(1));
+                    lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    
+    public List<Funcionario> ConsultaCodigoFuncionario(String nome) {
+        String sql = "select idfuncionario from funcionario where nome = '"+ nome +"'";
+        List<Funcionario> lista = new ArrayList<>();
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    Funcionario a = new Funcionario();
+                    a.setCod(rs.getInt(1));
+                    lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public boolean Testar_Funcionario(int cod) {
+            boolean Resultado = false;
+            try {
+                
+                String sql = "select * from funcionario where idfuncionario = " + cod + "";
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                
+                if (rs != null) {
+                    while (rs.next()) {
+                        Resultado = true;
+                    }
+                }
+            } catch (SQLException ex) {
+                ex.getMessage();
+            }
+            return Resultado;
+      }
+    
+    public List<Funcionario> ListarFuncionario() {
+        String sql = "select idfuncionario,nome,login,senha from funcionario";
+        List<Funcionario> lista = new ArrayList<>();
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs != null) {
+                while (rs.next()) {
+            Funcionario a = new Funcionario();
+            a.setCod(rs.getInt(1));
+            a.setNome(rs.getString(2));
+            a.setLogin(rs.getString(3));
+            a.setSenha(rs.getString(4));
+            
+            lista.add(a);
+                }
+                return lista;
+            } else {
+                return null;
+            }
+       } catch (SQLException e) {
+           return null; 
+       }
+   
+    }
+    
+    public String Excluir_Funcionario(Funcionario a){
+        String sql = "delete from funcionario where idfuncionario = ? and nome = ?";
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setInt(1, a.getCod());
+            ps.setString(2, a.getNome());
+            
+            if (ps.executeUpdate() > 0) {
+                return "Excluido com sucesso";
+            } else {
+                return "Erro ao excluir";
+            }
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
+    
+    public String Alterar_Funcionario(Funcionario a) {
+    String sql = "update cliente set nome = ? ,login = ? ,senha = ? ";
+    try {
+        PreparedStatement ps = getCon() .prepareStatement(sql);
+        ps.setString(1, a.getNome());
+        ps.setString(2, a.getLogin());
+        ps.setString(3, a.getSenha());
+        if (ps.executeUpdate() > 0) {
+            return "Atualizado com sucesso.";
+        } else {
+            return "Erro ao Atualizar";
+        }
+    } catch (SQLException e) {
+        return e.getMessage();
+    }
 }
 
+    public List<Funcionario> CapturarFuncionario(int cod) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
